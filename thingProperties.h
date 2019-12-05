32070ec3378b5fcb0438ae48be617a7ca3818ab2
ludviglundgrens <48127020+ludviglundgrens@users.noreply.github.com>
2019-12-05 18:11:40 +0100

    Update thingProperties.h

--- thingProperties.h
+++ thingProperties.h
@@ -7,18 +7,13 @@ const char THING_ID[] = "f743b30d-bd02-4e61-8a52-176c1f2a1b36";
 const char SSID[]     = SECRET_SSID;    // Network SSID (name)
 const char PASS[]     = SECRET_PASS;    // Network password (use for WPA, or use as key for WEP)
 
-void onLEDChange();
 
 float Ljussensor;
-bool LED;
-bool Knapp;
 
 void initProperties(){
 
   ArduinoCloud.setThingId(THING_ID);
-  ArduinoCloud.addProperty(Ljussensor, READ, ON_CHANGE, NULL, 20.000000);
-  ArduinoCloud.addProperty(LED, READWRITE, ON_CHANGE, onLEDChange);
-  ArduinoCloud.addProperty(Knapp, READ, ON_CHANGE, NULL);
+  ArduinoCloud.addProperty(Ljussensor, READ, ON_CHANGE, NULL, 10.000000);
 
 }
 
