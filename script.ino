7b4e4301eb3892882923b1b9ac38af7ed7fd3db4
ludviglundgrens <48127020+ludviglundgrens@users.noreply.github.com>
2019-12-05 18:11:19 +0100

    Update script.ino

--- script.ino
+++ script.ino
@@ -3,8 +3,6 @@
 
 
 #include "thingProperties.h"
-#define ledPin 0
-#define KnappPin A1
 #define BrightPin A2
 
 void setup() {
@@ -15,8 +13,6 @@ void setup() {
   // Connect to Arduino IoT Cloud
   ArduinoCloud.begin(ArduinoIoTPreferredConnection);
   
-  pinMode(ledPin, OUTPUT);
-  pinMode(KnappPin, INPUT);
   pinMode(BrightPin, INPUT);
   
   setDebugMessageLevel(2);
@@ -25,31 +21,11 @@ void setup() {
 
 void loop() {
   ArduinoCloud.update();
-  // from guide
-  //int brightSensor = analogRead(PIN_BRIGHT);
-  //brightness = 500 / (10.0 * (3.3 - (float)brightSensor / 1023 * 3.3) / ((float)brightSensor / 1023 * 3.3));
   
   // Your code here 
-  Knapp = digitalRead(KnappPin);
   Ljussensor = analogRead(BrightPin);
-  
-  
-  if(LED==1){
-    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  }
-  else{
-    digitalWrite(ledPin, LOW);
-  }
-
 }
 
-void onLEDChange() {
-  // Do something
-}
 void onLjussensorChange() {
-  // Do something
 }
 
-void onKnappChange() {
-  // Do something
-}
